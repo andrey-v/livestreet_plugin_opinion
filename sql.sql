f43e3f654023314b9958c9ee4ad642b37594befd
Андрей <mailgoods@mail.ru>
2012-10-14 09:11:42 +0400

    В таблицу БД добалено  IF NOT EXISTS

--- sql.sql
+++ sql.sql
@@ -9,7 +9,7 @@ PRIMARY KEY (`id`),
 KEY (`target_id`, `target_type`, `user_id`),
 KEY  `user_id` (`user_id`) );
 
-CREATE TABLE `prefix_opinion_rating` (
+CREATE TABLE IF NOT EXISTS `prefix_opinion_rating` (
   `user_id` INT NOT NULL,
   `user_rating` float(9,3) NOT NULL DEFAULT '0.000',
   `user_position` INT NOT NULL,
